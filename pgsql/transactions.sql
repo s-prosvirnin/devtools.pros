dbbb7d231dbb5f1c60b12e62df91dc24453e9503
s.prosvirnin <s-prosvirnin@ya.ru>
2021-05-20 02:14:53 +0300

    mysql, postgres

---
+++ pgsql/transactions.sql
@@ -0,0 +1,16 @@
+drop table if exists transactions;
+create table transactions (
+    id bigserial,
+    value int
+);
+
+-- select current_setting('transaction_isolation');
+
+-- concurrency update
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+update transactions set value = 6 where id = 1;
+commit;
+
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+update transactions set value = 7 where id = 1;
+commit;
